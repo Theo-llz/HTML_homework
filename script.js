f004fabe7b42939d858569ff2c92cf369b54e491
Lilian Tel <lilian.tel@etud.u-picardie.fr>
2020-03-19 20:04:43 +0100

    task5

--- script.js
+++ script.js
@@ -15,4 +15,11 @@ function init(){
 	let image = document.createElement("img");
 	image.src = "flower2.png";
 	document.getElementById("new_element").appendChild(image);
+	
+	let spans = document.getElementById("rainbow").getElementsByTagName("span");
+	let colors = ["red", "orange", "yellow", "green", "purple", "pink"];
+
+	for (var i = spans.length - 1; i >= 0; i--) {
+		spans[i].style.color = colors[i];
+	}
 }
