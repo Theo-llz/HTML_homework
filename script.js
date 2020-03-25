59f5f4dee6a5517cb9eb3842333627027b9bb1f4
Lilian Tel <lilian.tel@etud.u-picardie.fr>
2020-03-25 16:40:11 +0100

    Homework Solutions 2

--- script.js
+++ script.js
@@ -25,15 +25,6 @@ function removeItem(){
 }
 
 function init(){
-	document.getElementById("first").src = "flower1.png";
-	let images = document.getElementById("container").getElementsByTagName("img");
-	images[0].src = "flower1.png";
-	images[1].src = "flower2.png";
-	images[2].src = "flower1.png";
-	images[3].src = "flower2.png";
-	images[4].src = "flower1.png";
-	document.getElementById("text").innerHTML = "<div><p>Your are great!</p></div>";
-
 	let image = document.createElement("img");
 	image.src = "flower2.png";
 	document.getElementById("new_element").appendChild(image);
